1df4757a8b91cb6997a283b32b2285068c831d4a
Nawelle Congard <nawelle.congard@gmail.com>
2023-03-16 17:33:40 +0100

    watch list end day 1

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_directory ../rails-stylesheets-more-js .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 // app/assets/config/manifest.js
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, only: [:index, :show, :new, :create] do
+    get "bookmarks/new", to: "bookmarks#new"
+    post "bookmarks", to: "bookmarks#create"
+  end
+  delete "bookmarks/:id", to: "bookmarks#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
